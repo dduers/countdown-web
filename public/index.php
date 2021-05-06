01e0f6f72b26a4cddee5d02022f94b604ae8f9d5
Daniel Duersteler <daniel.duersteler@gmail.com>
2021-05-07 00:01:14 +0200

    .gitignore is now working

--- public/index.php
+++ public/index.php
@@ -2,17 +2,12 @@
 /**
  * MAIN FRONTEND CONTROLLER
  */
-
 // composer autoload
 require_once __DIR__."/../vendor/autoload.php";
-
 // get fatfree base instance
 $_f3 = \Base::instance();
-
 // load configuration files
-foreach (glob(__DIR__."/../app/config/*.ini") as $_inifile) {
+foreach (glob('../app/config/*.ini') as $_inifile)
     $_f3->config($_inifile);
-}
-
 // boot fatfree, listen for requests
 $_f3->run();
